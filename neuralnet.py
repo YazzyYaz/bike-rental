0af1b36634082332ba4b0c2eed0ee703f58947c1
Yaz Khoury <yaz.khoury@gmail.com>
2018-04-29 17:55:10 -0400

    feat: Add neuralnet initial file

---
+++ neuralnet.py
@@ -0,0 +1,9 @@
+class NeuralNetwork(object):
+    """Numpy Neural Network Class"""
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+        self.learning_rate = learning_rate
+
+
