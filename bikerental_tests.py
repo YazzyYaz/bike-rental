fbcabf7218979bade1f7fba042b9368d46d935a3
Yaz Khoury <yaz.khoury@gmail.com>
2018-04-29 18:41:48 -0400

    test: Integrate unittest suite

---
+++ bikerental_tests.py
@@ -0,0 +1,51 @@
+import unittest
+import neuralnet
+import dataset
+import pandas as pd
+import numpy as np
+
+inputs = np.array([[0.5, -0.2, 0.1]])
+targets = np.array([[0.4]])
+test_w_i_h = np.array([[0.1, -0.2],
+                       [0.4, 0.5],
+                       [-0.3, 0.2]])
+test_w_h_o = np.array([[0.3],
+                       [-0.1]])
+
+data = dataset.Dataset("data/hour.csv")
+
+
+class TestMethods(unittest.TestCase):
+    """Unit Tests for Data Loading"""
+    def test_data_path(self):
+        self.assertTrue(data.data_path == "data/hour.csv")
+
+    def test_data_load(self):
+        self.assertTrue(isinstance(data.rides, pd.DataFrame))
+
+    def test_activation(self):
+        network = neuralnet.NeuralNetwork(3, 2, 1, 0.5)
+        self.assertTrue(np.all(network.activation(0.5) == 1/(1+np.exp(-0.5))))
+
+    def test_train(self):
+        network = neuralnet.NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+        network.train(inputs, targets)
+
+        self.assertTrue(np.allclose(network.weights_hidden_to_output,
+                                    np.array([[0.37275328],
+                                              [-0.03172939]])))
+        self.assertTrue(np.allclose(network.weights_input_to_hidden,
+                                    np.array([[0.10562014, -0.20185996],
+                                              [0.39775194, 0.50074398],
+                                              [-0.29887597, 0.19962801]])))
+
+    def test_run(self):
+        network = neuralnet.NeuralNetwork(3, 2, 1, 0.5)
+        network.weights_input_to_hidden = test_w_i_h.copy()
+        network.weights_hidden_to_output = test_w_h_o.copy()
+        self.assertTrue(np.allclose(network.run(inputs), 0.09998924))
+
+suite = unittest.TestLoader().loadTestsFromModule(TestMethods())
+unittest.TextTestRunner().run(suite)
--- dataset.py
+++ dataset.py
@@ -12,12 +12,12 @@ class Dataset(object):
         self.__build_data_set()
 
     def __build_data_set(self):
-        rides = pd.read_csv(self.data_url)
+        self.rides = pd.read_csv(self.data_url)
         ## For categorical columns like months and days, we need to generate binary dummy variables
         for field in self.dummy_fields:
-            dummies = pd.get_dummies(rides[field], prefix=field, drop_first=False)
-            rides = pd.concat([rides, dummies], axis=1)
-        data = rides.drop(self.fields_to_drop, axis=1)
+            dummies = pd.get_dummies(self.rides[field], prefix=field, drop_first=False)
+            self.rides = pd.concat([self.rides, dummies], axis=1)
+        data = self.rides.drop(self.fields_to_drop, axis=1)
 
         ## Scaling the features to a mean of 0 and standard deviation of 1 for easier network training
         scaled_features = {}
