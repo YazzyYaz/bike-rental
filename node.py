a64fc38b5a16de0ba3707dce10544df2ee731986
Yaz Khoury <yaz.khoury@gmail.com>
2018-04-29 23:37:07 -0400

    feat: add input, output nodes

--- node.py
+++ node.py
@@ -1,2 +1,8 @@
 class Node(object):
-    def __init__(self):
+    def __init__(self, inbound_nodes=[]):
+        # Nodes from which this Node self will receive values
+        self.inbound_nodes = inbound_nodes
+        # Nodes from which this Node self will pass values
+        self.outbound_nodes = []
+        for n in self.inbound_nodes:
+            n.outbound_nodes.append(self)
