f4a41281131035e8a6b8d4fec3f9a0bbc5a430d9
Yaz Khoury <yaz.khoury@gmail.com>
2018-04-29 23:44:36 -0400

    feat: Add forward function, Input node class

--- node.py
+++ node.py
@@ -4,5 +4,29 @@ class Node(object):
         self.inbound_nodes = inbound_nodes
         # Nodes from which this Node self will pass values
         self.outbound_nodes = []
+        # For each inbound node, append this Node as outbound Node
         for n in self.inbound_nodes:
             n.outbound_nodes.append(self)
+        self.value = None
+
+    def forward(self):
+        """
+            Forward Propagation.
+
+            Compute the output value based on `inbound_nodes` and store
+            the result in self.value
+        """
+        raise NotImplemented
+
+class Input(Node):
+    def __init__(self):
+        # Input Node has no inbound_nodes
+        Node.__init__(self)
+
+    def forward(self, value=None):
+        """
+            Input Node is the only Node where we can pass self.value to
+            as argument to forward. Every other Node must rely on self.inbound_nodes
+        """
+        if value is not None:
+            self.value = value
